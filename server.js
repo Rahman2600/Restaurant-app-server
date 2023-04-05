95069b6e9b4586e3834bd8840dc9614f8ae1ac99
Abdurahman Mudasiru <abdurahman.mudasiru@sap.com>
2023-04-05 13:18:31 -0700

    Basic restaurant endpoints working

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+require("dotenv").config({ path: "./config.env" });
+
+const port = process.env.PORT || 6000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const restaurantsRouter = require("./routes/restaurants");
+
+app.use("/restaurants", restaurantsRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
